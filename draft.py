dfcbdef36ff44bb6fb5cc12654592e34edc6f7e1
mzdu <dan.du.pub@gmail.com>
2013-05-20 16:41:48 -0400

    re

--- draft.py
+++ draft.py
@@ -1,11 +1,7 @@
-list1 = ['b','c','d','e','f','g']
-list2 = ['b','c','d','e']
-list3 = ['a','k']
+import nltk
 
-# compare set1 with set2, set(list2) is a subset of set(list1), so say False
-print set(list1) < set(list2)
+str1 = 'this is s a jia jia jaj iJ IAJ IAJ I'
 
-# if we directly compare two lists, 
-# they will only compare the sequence of their first elements
-# since 'a' is less than 'b', then return True
-print list3 < list1
+#print len(str1)
+
+print sents(str1)
